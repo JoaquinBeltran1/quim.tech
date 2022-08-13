e3f6e666e405024c407a022bce93a46ebc1bdb7c
JoaquinBeltran1 <j.beltranbaldy@gmail.com>
2022-08-13 13:12:56 +0200

    pwogwess. First idea of header working

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,18 @@
 <script>
-      import "../app.css";
+    import "../app.css";
+    
+    let y = 0
+    $: condition = y <= 150;
+
+    // TODO: instead of arbitray 50, take bottom (or height) of hero element
 </script>
 
-<div class="bg-red-200">
+<svelte:window bind:scrollY={y}/>
+
+
+
+
+<div class:hidden={condition} class="bg-red-200 h-20 top-0 z-1 fixed w-full">
     header
     <div>
         <a href="/">
@@ -16,7 +26,21 @@
         </a>
     </div>
 </div>
-<slot/>
-<div class="bg-red-200">
+
+<div class="">
+    <slot/>
+
+</div>
+<h1 class:active={condition} class="h-screen">henlo world</h1>
+
+<div class="bg-red-200 h-44">
     footer
 </div>
+<style>
+    .hidden {
+        display: none;
+    }
+    .active {
+        color: brown
+    }
+</style>
--- src/routes/bldrdsh/bldrdsh_1.md
+++ src/routes/bldrdsh/bldrdsh_1.md
@@ -6,3 +6,4 @@ date: "2021-12-14"
 Hello, I am _Post One._ IN BLDRDSH!!!
 
 **Nice to meet you!**
+
